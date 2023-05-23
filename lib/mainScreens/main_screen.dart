bb449e10ba5bf98771d4930601baff3aaf81a06c
abiharinesh <abiharinesh@gmail.com>
2023-05-23 13:44:51 +0530

    first commit

---
+++ lib/mainScreens/main_screen.dart
@@ -0,0 +1,85 @@
+import 'package:drivers_app/tabpages/earning_tab.dart';
+import 'package:drivers_app/tabpages/home_tab.dart';
+import 'package:drivers_app/tabpages/profile_tab.dart';
+import 'package:drivers_app/tabpages/ratings_tab.dart';
+import 'package:flutter/material.dart';
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+
+
+
+class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin
+{
+
+  TabController? tabController;
+  int selectedIndex =0;
+
+
+  onItemClicked(int index)
+  {
+    setState(() {
+      selectedIndex = index;
+      tabController!.index = selectedIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: TabBarView(
+      physics: const  NeverScrollableScrollPhysics(),
+      controller: tabController,
+      children: const [
+        HomeTabPage(),
+        EarningsTabPage(),
+        RatingTabPage(),
+        ProfileTabPage(),
+      ],
+     ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+            BottomNavigationBarItem(
+              icon:Icon(Icons.home),
+              label: "Home",   
+            ),
+
+             BottomNavigationBarItem(
+              icon:Icon(Icons.credit_card),
+              label: "Earnings",   
+            ),
+
+             BottomNavigationBarItem(
+              icon:Icon(Icons.star),
+              label: "Rating",   
+            ),
+
+             BottomNavigationBarItem(
+              icon:Icon(Icons.person),
+              label: "Profile",   
+            ),
+        ],
+        unselectedItemColor: Colors.white38,
+        selectedItemColor: Colors.white,  
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(fontSize: 14),
+        showUnselectedLabels: true,
+        currentIndex: selectedIndex,
+        onTap: onItemClicked,
+        ),
+    );
+}
+}
---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'package:drivers_app/authentication/login_screen.dart';
+import 'package:drivers_app/authentication/signup_screen.dart';
+import 'package:drivers_app/global/global.dart';
+import 'package:drivers_app/mainScreens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen>
+ {
+  //startTimer is used to hold the current screen for given duration 
+  startTimer()
+  {
+    Timer(const Duration(seconds:3),()async
+    
+    {
+      if(await fAuth.currentUser !=null)
+      {
+
+        currentFirebaseUser = fAuth.currentUser;
+         Navigator.push(context, MaterialPageRoute(builder: (c)=> MainScreen()));
+      }
+      else
+      {
+         Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
+      }
+    });
+
+  }
+
+  @override
+  // initState runs first and calls the given functions   
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color:Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/logo.png"),
+              //add size boxes if needed
+              const Text(
+                "Rapido Clone App",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
